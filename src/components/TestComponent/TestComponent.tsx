d72a7ad60aeb1655669da179fd09429c93a13138
aavondet <aavondet@andrew.cmu.edu>
2020-09-07 11:37:10 -0400

    Starter code

---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+interface TestComponentProps {
+  greeting?: string
+  subject?: string
+}
+
+export const TestComponent: React.FC<TestComponentProps> = ({ greeting, subject }) => {
+  return (
+    <div>{greeting}, {subject}!</div>
+  );
+}
